2374744f07fc82013a1bf2906302bf429df85e56
Islam Attrash <isattrash@gmail.com>
2020-05-15 17:09:28 +0300

    final

--- stories/Counter/Strange/index.js
+++ stories/Counter/Strange/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import CounterContext from '../context';
+import connect from '../../../src/connect';
 
-const Strange = () => {
-    const { setContext, context } = React.useContext(CounterContext);
-    const { countx } = context;
-
-    const onClick = () => {
-        setContext('countx', 'id: ' + Math.random());
-    };
-
+const Strange = ({ countx, onClick }) => {
     return (
         <button onClick={onClick}>
             Update some value that Table do not refer to - current: {countx}
@@ -16,4 +9,15 @@ const Strange = () => {
     )
 };
 
-export default Strange;
+const useStateToProps = ({ context, setContext }) => {
+    const onClick = React.useCallback(() => {
+        setContext('countx', 'id: ' + Math.random());
+    }, [setContext]);
+
+    return {
+        countx: context.countx,
+        onClick
+    }
+};
+
+export default connect(useStateToProps)(Strange);
