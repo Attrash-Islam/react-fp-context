1b38df8306598ebeecc3e92c51e2c7b1aa0e21f4
Islam Attrash <isattrash@gmail.com>
2020-05-15 18:15:02 +0300

    first test!

--- src/spec.js
+++ src/spec.js
@@ -1,4 +1,39 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import identity from 'lodash/fp/identity';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import connect from './connect';
+import ReactFpContextProvider from '.';
 
-it('should pass', () => {
-    expect(true).toBe(true);
+configure({ adapter: new Adapter() });
+
+let currentContext;
+let currentsetContext;
+
+const Context = React.createContext();
+
+const ContextInspector = ({ context, setContext }) => {
+    currentContext = context;
+    currentsetContext = setContext;
+
+    return null;
+};
+
+const ConnectedContextInspector = connect(identity)(ContextInspector);
+
+const App = (options) => ReactFpContextProvider(options)(() => <ConnectedContextInspector/>);
+
+beforeEach(() => {
+    currentContext = null;
+    currentsetContext = null;
 });
+
+it('should build initial context value based on initial props', () => {
+    const Spec = App({ Context });
+    mount(<Spec count={1} name="islam"/>);
+
+    expect(currentContext).toEqual({ count: 1, name: 'islam' });
+});
+
+console.log(currentsetContext);
